162dc3bde75df8e88a8fa13e6659cfee61b4c18a
1220liang <martin20031220@gmail.com>
2025-12-06 00:54:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1483,7 +1483,7 @@ def handle_free_command(uid, text, replyTK):
         return
 
     # 7) 關鍵字搜尋
-     if is_keyword:
+    if is_keyword:
             lang = _get_lang(uid)
     
             # 中文與英文都對應到同一個 type
